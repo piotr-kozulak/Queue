0ff372325873b494e2cc7bbd598f20096878af9b
Piotrek <252760@edu.p.lodz.pl>
2024-05-16 08:56:54 +0200

    basic menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <stdio.h>
+void menu(void){
+    printf("\n");
+    printf("Enter a number into queue (push)\n");
+    printf("Read a value of the first element (top)\n");
+    printf("Delete first number (pop)\n");
+    printf("Check if queue is empty\n");
+    printf("Check if queue is full\n");
+    printf("End of program\n");
+    printf("\n");
+}
+int main(){
     return 0;
 }
